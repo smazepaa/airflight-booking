2a4dd75b8d6ddf61240bdcf083855a7e10b893df
smazepaa <smazepa@kse.ord.ua>
2023-11-06 22:47:12 +0200

    ConfigReader for reading the configuration file

--- booking/booking.cpp
+++ booking/booking.cpp
@@ -1,6 +1,57 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+
+class ConfigReader {
+
+    string filename;
+
+public:
+    ConfigReader(const string& filename) : filename(filename) {}
+
+    void readConfig() {
+        ifstream file(filename);
+        string line;
+        int num_records = 0;
+
+        if (file.is_open()) {
+            if (getline(file, line)) {
+                num_records = stoi(line);
+            }
+
+            for (int i = 0; i < num_records; i++) {
+                if (getline(file, line)) {
+                    istringstream iss(line);
+                    vector<string> words;
+                    string word;
+
+                    while (iss >> word) {
+                        words.push_back(word);
+                    }
+
+                    for (const auto& word : words) {
+                        cout << word << endl;
+                    }
+                }
+            }
+            file.close();
+        }
+        else {
+            cout << "Unable to open file" << '\n';
+        }
+    }
+};
+
+
+int main() {
+    
+    ConfigReader reader("config-input.txt");
+    reader.readConfig();
+
+    return 0;
 }
